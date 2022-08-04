00376d5ce3a13237c6f74356e7938805a7be5704
Arnold Southammavong <arnoldsouthammavong@Arnolds-MacBook-Pro-2021.local>
2022-08-04 17:43:00 -0500

    Add/update comments to fork

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 // Create a constant variable `express` and assign the imported `express` module to it
 const express = require("express");
+
+// Create a constant variable `app` and assign the `express` application to it
 const app = express();
+
+// Create a constant variable `MongoClient` and assign the imported `mongodb` module with the class `MongoClient` to it, using the `connect` method that came with the module
 const MongoClient = require("mongodb").MongoClient;
+
+// Create a constant variable `PORT` and assign the port number `2121` to it
 const PORT = 2121;
+
+// After importing the `dotenv` module, call the `config` method on it to load environment variables within the `.env` file into the `process.env` property
 require("dotenv").config();
 
+// Create and declare mutable variables `db`, `dbConnectionStr`, and `dbName`: `db` stores class instance; `dbConnectionStr` stores the connection string using process.env.DB_STRING (DB_STRING stored within .env file); `dbName` stores the name of the database we using
 let db,
   dbConnectionStr = process.env.DB_STRING,
   dbName = "todo";
 
+// Call static `connect` method on MongoClient with the object `dbConnectionStr` and its property `useUnifiedTopology` that has a value of `true` in order to use the server and engine
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
   (client) => {
+    // Logs string to the browser's console with `dbName` telling us we've connected to the database successfully
     console.log(`Connected to ${dbName} Database`);
+    // Re-assign `db` to `dbName` by using the current MongoClient instance's `db` method
     db = client.db(dbName);
   }
 );
