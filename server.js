9d50159287e1610bc292f7f0993e5c6194d2bf8e
Arnold Southammavong <arnoldsouthammavong@Arnolds-MacBook-Pro-2021.local>
2022-08-09 18:56:20 -0500

    Update index.ejs

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 // Create a constant variable `express` and assign the imported `express` module to it
-const express = require("express");
+const express = require('express');
 
 // Create a constant variable `app` and assign the `express` application to it
 const app = express();
 
 // Create a constant variable `MongoClient` and assign the imported `mongodb` module with the class `MongoClient` to it, using the `connect` method that came with the module
-const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
 // Create a constant variable `PORT` and assign the port number `2121` to it
 const PORT = 2121;
 
 // After importing the `dotenv` module, call the `config` method on it to load environment variables within the `.env` file into the `process.env` property
-require("dotenv").config();
+require('dotenv').config();
 
 // Create and declare mutable variables `db`, `dbConnectionStr`, and `dbName`: `db` stores class instance; `dbConnectionStr` stores the connection string using process.env.DB_STRING (DB_STRING stored within .env file); `dbName` stores the name of the database we using
 let db,
   dbConnectionStr = process.env.DB_STRING,
-  dbName = "todo";
+  dbName = 'todo';
 
 // Call static `connect` method on MongoClient with the object `dbConnectionStr` and its property `useUnifiedTopology` that has a value of `true` in order to use the server and engine
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
@@ -29,10 +29,10 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
 );
 
 // Call `set` method on our Express app's setting properties; `view engine` is the template engine to use, and `ejs` is the template
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // Call the Express app's `use` method to use middleware `express.static`, which is to serve static content for the app from the `public` directory in the application directory
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Call the Express app's `use` method to use middleware function `express.urlencoded`, which is to parse incoming requests with urlencoded payloads and is based on `body-parser`; aka adds `body-parser` `urlencoded` middleware to the Express app, and parses the content requests `Content-Type` with the type "application/x-www-form-urlencoded"
 // From Express docs: Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
@@ -42,17 +42,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Use `get` method to create a custom request handler at the path `/`
-app.get("/", async (request, response) => {
+app.get('/', async (request, response) => {
   // Create a variable `todoItems`, assigning to it an array of document objects within the connected database's collection; at the database's collection, we use find() with no filter object to obtain all documents, and then we use toArray() to convert document query into an array
-  const todoItems = await db.collection("todos").find().toArray();
+  const todoItems = await db.collection('todos').find().toArray();
 
   // Create a variable `itemsLeft`, assigning to it an array of completed document objects within the connected database's collection; at the database's collection, we use countDocuments() with the property `completed` and value of `false` to obtain only documents that haven't been completed
   const itemsLeft = await db
-    .collection("todos")
+    .collection('todos')
     .countDocuments({ completed: false });
 
   // Renders our view using `index.ejs` with the variables `todoItems` and `itemsLeft`
-  response.render("index.ejs", { items: todoItems, left: itemsLeft });
+  response.render('index.ejs', { items: todoItems, left: itemsLeft });
 
   // db.collection('todos').find().toArray()
   // .then(data => {
@@ -65,23 +65,23 @@ app.get("/", async (request, response) => {
 });
 
 // Use `post` method to create a custom request handler at the path `/addTodo`
-app.post("/addTodo", (request, response) => {
+app.post('/addTodo', (request, response) => {
   // Within the collection, insertOne() is used to call objects with the properties `thing` and `completed`, with `request.body.todoItem` and `false` as values for those properties, respectively
-  db.collection("todos")
+  db.collection('todos')
     .insertOne({ thing: request.body.todoItem, completed: false })
     // After insertOne() method, the page redirects to the path at `/`
     .then((result) => {
-      console.log("Todo Added");
-      response.redirect("/");
+      console.log('Todo Added');
+      response.redirect('/');
     })
     // If insertOne() method fails, we log the error to the browser console
     .catch((error) => console.error(error));
 });
 
 // Use `put` method to update a custom request handler at the path `/markComplete`
-app.put("/markComplete", (request, response) => {
+app.put('/markComplete', (request, response) => {
   // Within the collection, updateOne() is used to update object properties `thing` to the value at `request.body.itemFromJS` that was parsed from the `json` middleware
-  db.collection("todos")
+  db.collection('todos')
     .updateOne(
       { thing: request.body.itemFromJS },
       // `$set` is the Update Operator used to update the `completed` property value to `true`
@@ -99,17 +99,17 @@ app.put("/markComplete", (request, response) => {
     )
     // After updateOne() method, the document is marked as completed
     .then((result) => {
-      console.log("Marked Complete");
-      response.json("Marked Complete");
+      console.log('Marked Complete');
+      response.json('Marked Complete');
     })
     // If updateOne() method fails, we log the error to the browser console
     .catch((error) => console.error(error));
 });
 
 // Use `put` method to update a custom request handler at the path `/markUnComplete`
-app.put("/markUnComplete", (request, response) => {
+app.put('/markUnComplete', (request, response) => {
   // Within the collection, updateOne() is used to update object properties `thing` to the value at `request.body.itemFromJS` that was parsed from the `json` middleware
-  db.collection("todos")
+  db.collection('todos')
     .updateOne(
       { thing: request.body.itemFromJS },
       // `$set` is the Update Operator used to update the `completed` property value to `false`
@@ -127,22 +127,22 @@ app.put("/markUnComplete", (request, response) => {
     )
     // After updateOne() method, the document is marked as uncomplete
     .then((result) => {
-      console.log("Marked Complete");
-      response.json("Marked Complete");
+      console.log('Marked Complete');
+      response.json('Marked Complete');
     })
     // If updateOne() method fails, we log the error to the browser console
     .catch((error) => console.error(error));
 });
 
 // Use `delete` method to update a custom request handler at the path `/deleteItem`
-app.delete("/deleteItem", (request, response) => {
+app.delete('/deleteItem', (request, response) => {
   // Within the collection, deleteOne() is used to update object properties `thing` to the value at `request.body.itemFromJS` that was parsed from the `json` middleware
-  db.collection("todos")
+  db.collection('todos')
     .deleteOne({ thing: request.body.itemFromJS })
     // After deleteOne() method, the document has been deleted
     .then((result) => {
-      console.log("Todo Deleted");
-      response.json("Todo Deleted");
+      console.log('Todo Deleted');
+      response.json('Todo Deleted');
     })
     // If deleteOne() method fails, we log the error to the browser console
     .catch((error) => console.error(error));
